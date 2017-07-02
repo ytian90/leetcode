0d5c8f8d0072c7ed1362f50b821d0af635b75bd3
ytian90 <tianyu.ouc@gmail.com>
2017-07-02 00:36:06 -0700

    551

---
+++ src/string/StudentAttendanceRecord.java
@@ -0,0 +1,22 @@
+package string;
+/**
+ * 551. Student Attendance Record I
+ * @author ytian
+ *
+ */
+public class StudentAttendanceRecord {
+	
+	public static boolean checkRecord(String s) {
+		return !(s.indexOf("A") != s.lastIndexOf("A") || s.contains("LLL"));
+    }
+	
+	public static boolean checkRecord2(String s) {
+		return !s.matches(".*LLL.*|.*A.*A.*");
+	}
+
+	public static void main(String[] args) {
+		System.out.println(checkRecord("PPALLP"));
+		System.out.println(checkRecord("PPALLL"));
+	}
+
+}
