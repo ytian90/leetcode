0c648c791cfaec6689cf8fb6f6cc101898921763
ytian90 <tianyu.ouc@gmail.com>
2017-07-02 22:59:47 -0700

    629 568

--- src/greedy/CourseSchedule3.java
+++ src/greedy/CourseSchedule3.java
@@ -13,12 +13,14 @@ public class CourseSchedule3 {
 	public static int scheduleCourse(int[][] courses) {
         int n = courses.length;
         if (n == 0) return 0;
+        // Sort the courses by their deadlines (Greedy! We have to deal with courses with early deadlines first)
         Arrays.sort(courses, (a, b) -> a[1] - b[1]);
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         int start = 0;
         for (int[] c : courses) {
-        	start += c[0];
+        	start += c[0]; // add current course to a priority queue
         	pq.offer(c[0]);
+        	// If time exceeds, drop the previous course which costs the most time.
         	if (start > c[1]) {
         		start -= pq.poll();
         	}
