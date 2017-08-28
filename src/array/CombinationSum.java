8b0d98330862a4ac883cad0102ed5db38f3b2590
ytian90 <tianyu.ouc@gmail.com>
2017-08-27 22:45:37 -0700

    update 39 & test case

--- src/array/CombinationSum.java
+++ src/array/CombinationSum.java
@@ -10,23 +10,23 @@ import java.util.List;
  * @since Aug 15, 2015
  */
 public class CombinationSum {
-	private static List<List<Integer>> res = new ArrayList<List<Integer>>();
-	private static List<Integer> list = new ArrayList<Integer>();
 	
 	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> list = new ArrayList<Integer>();
 		Arrays.sort(candidates);
-		helper(candidates, target, 0);
+		helper(candidates, target, 0, list, res);
 		return res;
 	}
 
-	private static void helper(int[] c, int target, int start) {
+	private static void helper(int[] c, int target, int start, List<Integer> list, List<List<Integer>> res) {
 		if (target < 0) return;
 		else if (target == 0) {
 			res.add(new ArrayList<Integer>(list));
 		} else {
 			for (int i = start; i < c.length; i++) {
 				list.add(c[i]);
-				helper(c, target - c[i], i);
+				helper(c, target - c[i], i, list, res);
 				list.remove(list.size() - 1);
 			}
 		}
@@ -34,5 +34,6 @@ public class CombinationSum {
 	
 	public static void main(String[] args) {
 		System.out.println(combinationSum(new int[]{1}, 2));
+		System.out.println(combinationSum(new int[]{2, 3, 6, 7}, 7));
 	}
 }
