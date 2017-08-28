300383c9d3b092cbb94748e1dfe0bcc7ff043b88
ytian90 <tianyu.ouc@gmail.com>
2017-08-27 22:25:24 -0700

    update 57 test case

--- src/sort/InsertInterval.java
+++ src/sort/InsertInterval.java
@@ -1,30 +1,19 @@
 package sort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
- * Insert Interval
+ * 57. Insert Interval
  * @author yutian
  * @since Aug 30, 2015
  */
 public class InsertInterval {
-	public class Interval {
-		int start;
-		int end;
-		Interval() { start = 0; end = 0; }
-		Interval(int s, int e) { start = s; end = e; }
-	}
 	
-	public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+	public static List<Interval> insert(List<Interval> intervals, Interval newInterval) {
 		// if intervals is not sorted.
-//		Collections.sort(intervals, new Comparator<Interval>(){
-//			public int compare(Interval a, Interval b) {
-//				return a.start - b.start;
-//			}
-//		});
 		Collections.sort(intervals, (a, b) -> a.start - b.start);
         List<Interval> res = new ArrayList<>();
         for (int i = 0; i < intervals.size(); i++) {
@@ -44,6 +33,32 @@ public class InsertInterval {
 	}
 	
 	public static void main(String[] args) {
+		Interval a = new Interval(1, 3);
+		Interval b = new Interval(6, 9);
+		Interval c = new Interval(2, 5);
+		List<Interval> t = new ArrayList<>(Arrays.asList(a, b));
+		for (Interval i : insert(t, c)) {
+			System.out.println(i.start + " " + i.end);
+		}
 		
+		System.out.println();
+		
+		Interval aa = new Interval(1, 2);
+		Interval bb = new Interval(3, 5);
+		Interval cc = new Interval(6, 7);
+		Interval dd = new Interval(8, 10);
+		Interval ee = new Interval(12, 16);
+		Interval ff = new Interval(4, 9);
+		List<Interval> tt = new ArrayList<>(Arrays.asList(aa, bb, cc, dd, ee));
+		for (Interval i : insert(tt, ff)) {
+			System.out.println(i.start + " " + i.end);
+		}
+	}
+	
+	public static class Interval {
+		int start;
+		int end;
+		Interval() { start = 0; end = 0; }
+		Interval(int s, int e) { start = s; end = e; }
 	}
 }
