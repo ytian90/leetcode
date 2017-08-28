a0c580f498a941820bc2609d379612f5c8909a5f
ytian90 <tianyu.ouc@gmail.com>
2017-08-27 22:03:11 -0700

    update 56

--- src/sort/MergeIntervals.java
+++ src/sort/MergeIntervals.java
@@ -11,41 +11,33 @@ import java.util.List;
  * @since Aug 30, 2015
  */
 public class MergeIntervals {
-	public class Interval {
-		int start;
-		int end;
-		Interval() { start = 0; end = 0; }
-		Interval(int s, int e) { start = s; end = e; }
-	}
 	
 	public List<Interval> merge(List<Interval> intervals) {
 		if (intervals.size() <= 1) return intervals;
-		
-//		// Sort by ascending starting point using an anonymous Comparator
-//		Collections.sort(intervals, new Comparator<Interval>() {
-//			@Override
-//			public int compare(Interval i1, Interval i2) {
-//				return Integer.compare(i1.start, i2.start);
-//			}
-//		});
-		
 		Collections.sort(intervals, (a, b) -> a.start - b.start);
 		
-		List<Interval> result = new LinkedList<Interval>();
+		List<Interval> res = new LinkedList<Interval>();
 		int start = intervals.get(0).start;
 		int end = intervals.get(0).end;
 		for (Interval i: intervals) {
             if (end >= i.start) { // Overlapping intervals, move the end if needed
                 end = Math.max(end, i.end);
             } else { // Disjoint intervals, add the previous one and reset bounds
-                result.add(new Interval(start, end));
+                res.add(new Interval(start, end));
                 start = i.start;
                 end = i.end;
             }
         }
 		
 		// Add the last interval
-		result.add(new Interval(start, end));
-		return result;
+		res.add(new Interval(start, end));
+		return res;
+	}
+	
+	public class Interval {
+		int start;
+		int end;
+		Interval() { start = 0; end = 0; }
+		Interval(int s, int e) { start = s; end = e; }
 	}
 }
