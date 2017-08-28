21b2f3730cec130c37caada7b2da40ee799ed261
ytian90 <tianyu.ouc@gmail.com>
2017-08-27 21:46:55 -0700

    update 452

--- src/greedy/MinimumNumberOfArrowsToBurstBalloons.java
+++ src/greedy/MinimumNumberOfArrowsToBurstBalloons.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 /**
  * 452. Minimum Number of Arrows to Burst Balloons
+ * Same idea as meeting room
  * @author yutian
  *
  */
 public class MinimumNumberOfArrowsToBurstBalloons {
 	
-	public int findMinArrowShots(int[][] points) {
+	public static int findMinArrowShots(int[][] points) {
 		if (points == null || points.length < 1) return 0;
 		Arrays.sort(points, (a, b) -> (a[0] - b[0]));
 		int res = 1;
@@ -26,8 +27,14 @@ public class MinimumNumberOfArrowsToBurstBalloons {
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] t = new int[][]{
+			{10, 16},
+			{2, 8},
+			{1, 6},
+			{7, 12}
+		};
+		
+		System.out.println(findMinArrowShots(t));
 	}
 
 }
