b51ab619bf40ee27247cb787b5eddd1d6b267d06
ytian90 <tianyu.ouc@gmail.com>
2017-09-03 19:59:21 -0700

    update 424

--- src/string/LongestRepeatingCharacterReplacement.java
+++ src/string/LongestRepeatingCharacterReplacement.java
@@ -7,21 +7,19 @@ package string;
 public class LongestRepeatingCharacterReplacement {
 	
 	public static int characterReplacement(String s, int k) {
-        int len = s.length();
         int[] count = new int[26];
-        int start = 0, max = 0, res = 0;
-        for (int end = 0; end < len; end++) {
-        	max = Math.max(max, ++count[s.charAt(end) - 'A']);
-        	while (end - start + 1 - max > k) {
-        		count[s.charAt(start++) - 'A']--;
+        int prev = 0, max = 0, res = 0;
+        for (int i = 0; i < s.length(); i++) {
+        	max = Math.max(max, ++count[s.charAt(i) - 'A']);
+        	while (i - prev + 1 - max > k) {
+        		count[s.charAt(prev++) - 'A']--;
         	}
-        	res = Math.max(res, end - start + 1);
+        	res = Math.max(res, i - prev + 1);
         }
         return res;
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(characterReplacement("ABAB", 2));
 		System.out.println(characterReplacement("AABABBA", 1));
 	}
