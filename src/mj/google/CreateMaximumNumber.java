586dd11718141ee04cd55e320dcb4bda7890bb6f
ytian90 <tianyu.ouc@gmail.com>
2019-11-06 13:46:51 -0800

    google

---
+++ src/mj/google/CreateMaximumNumber.java
@@ -0,0 +1,45 @@
+package mj.google;
+
+import java.util.Arrays;
+
+/**
+ * lc321
+ */
+public class CreateMaximumNumber {
+    public static int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] res = new int[k];
+        for (int i = Math.max(0, k - m); i <= k && i <= n; i++) {
+            int[] candidate = merge(maxArray(nums1, i), maxArray(nums2, k - i), k);
+            if (greater(candidate, 0, res, 0)) res = candidate;
+        }
+        return res;
+    }
+    private static int[] merge(int[] nums1, int[] nums2, int k) {
+        int[] res = new int[k];
+        for (int i = 0, j = 0, r = 0; r < k; r++)
+            res[r] = greater(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
+        return res;
+    }
+    public static boolean greater(int[] nums1, int i, int[] nums2, int j) {
+        while (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
+            i++;
+            j++;
+        }
+        return j == nums2.length || (i < nums1.length && nums1[i] > nums2[j]);
+    }
+    public static int[] maxArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[k];
+        for (int i = 0, j = 0; i < n; i++) {
+            while (n - i + j > k && j > 0 && res[j - 1] < nums[i]) j--;
+            if (j < k) res[j++] = nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(maxArray(new int[]{3, 4, 6, 5}, 2)));
+    }
+}
---
+++ src/mj/google/TheMaze2.java
@@ -0,0 +1,51 @@
+package mj.google;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * lc505
+ */
+public class TheMaze2 {
+    class Point {
+        int x, y, l;
+        public Point(int x, int y, int l) {
+            this.x = x;
+            this.y = y;
+            this.l = l;
+        }
+    }
+
+    private int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public int shortestDistance(int[][] maze, int[] start, int[] dest) {
+        int n = maze.length, m = maze[0].length;
+        int[][] len = new int[n][m];
+        for (int[] i : len) {
+            Arrays.fill(i, Integer.MAX_VALUE);
+        }
+        PriorityQueue<Point> pq = new PriorityQueue<>((a, b) -> (a.l - b.l));
+        pq.offer(new Point(start[0], start[1], 0));
+        while (!pq.isEmpty()) {
+            Point p = pq.poll();
+            if (len[p.x][p.y] < p.l) {
+                continue;
+            }
+            len[p.x][p.y] = p.l;
+            for (int[] d : dirs) {
+                int x = p.x, y = p.y, l = p.l;
+                while (x >= 0 && x < n && y >= 0 && y < m && maze[x][y] == 0) {
+                    x += d[0];
+                    y += d[1];
+                    l++;
+                }
+                x -= d[0];
+                y -= d[1];
+                l--;
+                pq.offer(new Point(x, y, l));
+            }
+        }
+        return len[dest[0]][dest[1]] == Integer.MAX_VALUE
+                ? -1 : len[dest[0]][dest[1]];
+    }
+}
