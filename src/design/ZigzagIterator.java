0842f7cee931a293eaf4be1ee4a3e34149963f53
ytian90 <tianyu.ouc@gmail.com>
2019-11-05 16:33:19 -0800

    google

--- src/design/ZigzagIterator.java
+++ src/design/ZigzagIterator.java
@@ -1,9 +1,6 @@
 package design;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 281. Zigzag Iterator
@@ -12,18 +9,18 @@ import java.util.List;
  */
 public class ZigzagIterator {
 	// Solution 1
-	public LinkedList<Iterator> list;
+	public Queue<Iterator> list;
 	
 	public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        list = new LinkedList<Iterator>();
+        list = new LinkedList<>();
         if (!v1.isEmpty()) list.add(v1.iterator());
         if (!v2.isEmpty()) list.add(v2.iterator());
     }
 
     public int next() {
-        Iterator poll = list.remove();
-        int res = (int) poll.next();
-        if (poll.hasNext()) list.add(poll);
+        Iterator curr = list.poll();
+        int res = (int) curr.next();
+        if (curr.hasNext()) list.add(curr);
         return res;
     }
 
@@ -32,9 +29,9 @@ public class ZigzagIterator {
     }
 
 	public static void main(String[] args) {
-		List<Integer> v1 = new ArrayList<Integer>();
+		List<Integer> v1 = new ArrayList<>();
 		v1.add(1); v1.add(2);
-		List<Integer> v2 = new ArrayList<Integer>();
+		List<Integer> v2 = new ArrayList<>();
 		v2.add(3); v2.add(4); v2.add(5); v2.add(6);
 		
 		ZigzagIterator i = new ZigzagIterator(v1, v2);
---
+++ src/mj/google/IteratorOfIterator.java
@@ -0,0 +1,39 @@
+package mj.google;
+
+import design.ZigzagIterator;
+
+import java.util.*;
+
+/**
+ * lc281
+ */
+public class IteratorOfIterator {
+    public Queue<Iterator> list;
+
+    public IteratorOfIterator(List<Integer> v1, List<Integer> v2) {
+        list = new LinkedList<>();
+        if (!v1.isEmpty()) list.add(v1.iterator());
+        if (!v2.isEmpty()) list.add(v2.iterator());
+    }
+
+    public int next() {
+        Iterator curr = list.poll();
+        int res = (int) curr.next();
+        if (curr.hasNext()) list.add(curr);
+        return res;
+    }
+
+    public boolean hasNext() {
+        return !list.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> v1 = new ArrayList<>();
+        v1.add(1); v1.add(2);
+        List<Integer> v2 = new ArrayList<>();
+        v2.add(3); v2.add(4); v2.add(5); v2.add(6);
+
+        ZigzagIterator i = new ZigzagIterator(v1, v2);
+        while (i.hasNext()) System.out.print(i.next() + " ");
+    }
+}
