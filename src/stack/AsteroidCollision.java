19636a1c3b1d7b4fc141637588d3e3d3dd587fb0
ytian90 <tianyu.ouc@gmail.com>
2020-05-09 23:15:42 -0700

    update google youtube video question

--- src/stack/AsteroidCollision.java
+++ src/stack/AsteroidCollision.java
@@ -1,13 +1,54 @@
 package stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
  * 735. Asteroid Collision
  */
 public class AsteroidCollision {
-
     public static int[] asteroidCollision(int[] asteroids) {
+        if (asteroids == null || asteroids.length == 0) {
+            return null;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < asteroids.length; i++) {
+            if (asteroids[i] > 0) {
+                stack.push(asteroids[i]);
+            } else {
+                if (stack.isEmpty()) {
+                    stack.push(asteroids[i]);
+                } else {
+                    while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroids[i])) {
+                        stack.pop();
+                    }
+                    if (stack.isEmpty() || stack.peek() < 0) {
+                        stack.push(asteroids[i]);
+                        continue;
+                    } else if (stack.peek() == Math.abs(asteroids[i])) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        int size = stack.size();
+        int[] res = new int[size];
+        for (int i = size - 1; i >= 0; i--) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{5, 10, -5})));
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{5, -5})));
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{10, 2, -5})));
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{-2, -1, 1, 2})));
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{-2, -2, 1, -2})));
+        System.out.println(Arrays.toString(asteroidCollision(new int[]{1, -2, -2, -2})));
+    }
+
+    public static int[] asteroidCollision2(int[] asteroids) {
         if (asteroids == null || asteroids.length == 0)
             return new int[]{};
         if (asteroids.length < 2)
@@ -27,12 +68,5 @@ public class AsteroidCollision {
             }
         }
         return stack.stream().mapToInt(i -> i).toArray();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(asteroidCollision(new int[]{5, 10, -5}));
-        System.out.println(asteroidCollision(new int[]{9, -9}));
-        System.out.println(asteroidCollision(new int[]{10, 2, -5}));
-        System.out.println(asteroidCollision(new int[]{-2, -1, 1, 2}));
     }
 }
